f43ddcaab767a21aad8e069e7f013c15bdae8d1a
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-09-05 21:24:42 +0530

    created basic layout for homepage header

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <main className="w-full h-full px-16 py-20">
+      <div className="w-full inline-flex justify-between">
+        <h1 className="font-bold text-3xl">Here are your yummy recipes!</h1>
+        <button>Add Recipe</button>
+      </div>
+    </main>
+  );
 }
 
 export default Home;
