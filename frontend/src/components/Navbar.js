02c3f6ca3518153ada7787185dca925eb6808525
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-09-06 20:56:06 +0530

    added  refresh button to navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "./Button";
 
 function Navbar() {
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
   return (
-    <header className="w-full px-6 py-2 bg-orange-400">
+    <header className="w-full px-6 py-2 bg-orange-400 flex items-center justify-between">
       <Link to="/">Mern Recipes</Link>
-      <div></div>
+      <div>
+        <Button handleOnClick={refreshPage}>Refresh Page</Button>
+      </div>
     </header>
   );
 }
