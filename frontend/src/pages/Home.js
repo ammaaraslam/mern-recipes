cf6ed7b5a47f8aed03ac44403e0108395c7ea8ca
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-09-06 08:39:36 +0530

    added route change to add recipe button in homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,16 +2,18 @@ import React from "react";
 import Button from "../components/Button";
 import { useSelector } from "react-redux";
 import RecipeCard from "../components/RecipeCard";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   const recipes = useSelector((state) => state.recipes);
+  const navigate = useNavigate();
   console.log(recipes);
 
   return (
     <main className="w-full h-full px-16 py-20">
       <div className="w-full inline-flex justify-between">
         <h1 className="font-bold text-3xl">Here are your yummy recipes!</h1>
-        <Button>Add Recipe</Button>
+        <Button handleOnClick={() => navigate("/add")}>Add Recipe</Button>
       </div>
       <div className="mx-auto py-14 px-7 flex flex-wrap gap-20 items-center justify-center">
         {recipes.map((recipe) => (
